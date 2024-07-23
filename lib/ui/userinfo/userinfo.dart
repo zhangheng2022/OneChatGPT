f258bbcb0337dd8964931510c7fc4a7a9183637c
weiyang <962271461@qq.com>
2024-07-23 19:40:56 +0800

    commit init

---
+++ lib/ui/userinfo/userinfo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Userinfo extends StatefulWidget {
+  const Userinfo({super.key});
+  @override
+  State<Userinfo> createState() => _Userinfo();
+}
+
+class _Userinfo extends State<Userinfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("123");
+  }
+}
