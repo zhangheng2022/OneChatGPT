e9ea5a9e419305309deaed587b029b9e8eaef7ce
weiyang <962271461@qq.com>
2024-08-01 19:03:33 +0800

    commit init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:one_chatgpt_flutter/database/database.dart';
+import 'package:one_chatgpt_flutter/global_window_listener.dart';
 import 'package:one_chatgpt_flutter/state/model_config.dart';
 import 'package:provider/provider.dart';
 import 'package:one_chatgpt_flutter/router.dart';
@@ -8,7 +10,6 @@ import 'package:one_chatgpt_flutter/state/auth.dart';
 import 'package:one_chatgpt_flutter/screen.dart';
 import 'package:one_chatgpt_flutter/common/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   await Screen.initialize();
@@ -34,90 +35,33 @@ class RunApp extends StatelessWidget {
           lazy: false,
         ),
       ],
-      child: GlobalWindowListener(
-        child: MaterialApp.router(
-          title: 'OneChatGPT',
-          themeMode: ThemeMode.light,
-          theme: GlobalTheme.lightThemeData,
-          darkTheme: GlobalTheme.darkThemeData,
-          debugShowCheckedModeBanner: false,
-          routerConfig: AppRoutes.router,
-          builder: FlutterSmartDialog.init(),
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale.fromSubtags(languageCode: 'zh'),
-          ],
-          localeResolutionCallback: (
-            locale,
-            supportedLocales,
-          ) {
-            return locale;
-          },
-        ),
+      child: MaterialApp.router(
+        title: 'OneChatGPT',
+        themeMode: ThemeMode.light,
+        theme: GlobalTheme.lightThemeData,
+        darkTheme: GlobalTheme.darkThemeData,
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRoutes.router,
+        builder: (context, child) {
+          child = FlutterSmartDialog.init()(context, child);
+          child = GlobalWindowListener(child: child);
+          return child;
+        },
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale.fromSubtags(languageCode: 'zh'),
+        ],
+        localeResolutionCallback: (
+          locale,
+          supportedLocales,
+        ) {
+          return locale;
+        },
       ),
     );
-  }
-}
-
-class GlobalWindowListener extends StatefulWidget {
-  final Widget child;
-
-  const GlobalWindowListener({super.key, required this.child});
-
-  @override
-  State<GlobalWindowListener> createState() => _GlobalWindowListener();
-}
-
-class _GlobalWindowListener extends State<GlobalWindowListener>
-    with WindowListener {
-  @override
-  void initState() {
-    super.initState();
-    windowManager.addListener(this);
-  }
-
-  @override
-  void dispose() {
-    windowManager.removeListener(this);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.child;
-  }
-
-  @override
-  Future<void> onWindowClose() async {
-    bool isPreventClose = await windowManager.isPreventClose();
-    if (!isPreventClose) return;
-    bool? confirmed = await showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('确认退出？'),
-          content: const Text('确定要退出当前账号吗？'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(false),
-              child: const Text('取消'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(true),
-              child: Text(
-                '确定',
-                style: TextStyle(color: Theme.of(context).colorScheme.error),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-
-    if (confirmed == true) {}
   }
 }
